24f93ba4bbd1a66586a911ca8f30dd0a51bf5127
Tejas01-developes <tlimbad7@gmail.com>
2026-02-10 22:50:11 +0530

    access token learning

--- react8/src/Userpages/Home.jsx
+++ react8/src/Userpages/Home.jsx
@@ -1,12 +1,33 @@
 import React, { useContext, useEffect } from 'react'
 import { authcontext } from '../Auth/Auth'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
+
+
+  const navigate=useNavigate();
     const{access,setaccess}=useContext(authcontext);
-    
-    useEffect(()=>{
-        const newacc=async()=>{
+
+//     function decodejwt(token){
+//       if(!token){
+//         return console.log("no token")
+//       }
+//   const payload=token.split(".")[1];
+//   const decode=JSON.parse(atob(payload));
+//   return decode;
+// }
+// const decodeaccess=decodejwt(access);
+// console.log(decodeaccess);
+const noaccess=()=>{
+if(!access){
+  return navigate("/")
+}
+}
+
+    useEffect(()=>{  
+      const newacc=async()=>{
             const accurl=await axios.post("http://localhost:3000/apis/newacc",{},{withCredentials:true});
             if(accurl.data.success){
                 setaccess(accurl.data.access)
