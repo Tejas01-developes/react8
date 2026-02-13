367eed65ec5449b80deca9ebeb45a5a7b00ff6d3
Tejas01-developes <tlimbad7@gmail.com>
2026-02-13 16:43:48 +0530

    added to upload image on s3

--- react8/src/Userpages/Home.jsx
+++ react8/src/Userpages/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { authcontext } from '../Auth/Auth'
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+
 
 
 const Home = () => {
 
 
-  const navigate=useNavigate();
+
     const{access,setaccess}=useContext(authcontext);
     const [loading,setloading]=useState(true)
     const[field,setfield]=useState({file:null,name:""})
@@ -31,6 +31,7 @@ const handlefile=(e)=>{
             const accurl=await axios.post("http://localhost:3000/apis/newacc",{},{withCredentials:true});
             if(accurl.data.success){
                 setaccess(accurl.data.access)
+                console.log(accurl.data.access)
                 setloading(false)
             }
           
@@ -45,27 +46,38 @@ const handlefile=(e)=>{
     },[])
 
 
-const uploadfile=()=>{
+const uploadfile=async()=>{
 if(!field.file){
   return alert("select the photo")
 }
 const formdata=new FormData();
 const filename=field.name ? field.name : field.file.name;
-    formdata.append("filename",field.file);
-    formdata.append("name",filename)
-}
-    
+    formdata.append("file",field.file);
+    formdata.append("name",filename);
 
-    
+    const uploadurl=await axios.post("http://localhost:3000/images/upload",formdata,{withCredentials:true,headers:{Authorization:`Bearer ${access}`}});
+    if(uploadurl.data.success){
+      return alert("upload success")
+    }
+    alert(uploadurl.data.message)
+
+}
+
+
 
   return (
     <div>
-
+<div>
   {loading ? <h2>Loading........</h2> : <h1>{access}</h1>}
 
-   <input type="file" value={field.file} onChange={handlefile} name='file'/>
+   <input type="file"  onChange={handlefile} name='file'/>
    <input type="text" placeholder='Filename' value={field.name} onChange={handlefile} name='name'/>
+   <button onClick={uploadfile}>upload</button>
     </div>
+
+  
+
+</div>
   )
 }
 
